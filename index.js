87ce8a3f4383048e1499ed6677785e9ae57eb734
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-07 21:43:50 -0700

    sum fromN to toN

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-
-function sum(fromN, toN) {
+const sum = (fromN, toN) => {
   // Sum all the values from fromN up to toN
-}
+  if (fromN >= toN) return fromN;
+  return fromN + sum(++fromN, toN);
+};
 
 module.exports = sum;
